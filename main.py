295e0b7301bf7ac70ead8b05465f230e615689cb
joci <jozsef.petenyi@gmail.com>
2023-08-04 23:20:36 -0700

    - Added logging for the results

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import matplotlib.pyplot as plt
-
 from game import Game
 
 if __name__ == '__main__':
@@ -14,8 +12,16 @@ if __name__ == '__main__':
     print(f'Remaining: {result[-1].total_number_of_chips} chips')
 
     # todo how is this defined?
-    edge = (result[-1].total_number_of_chips - result[0].total_number_of_chips) / result[-1].round
-    print(f'Edge: {edge * 100:.3f} %')
+    total_number_of_rounds = (result[-1].round - result[0].round)  # because it can end early
+    number_of_chips_gained = (result[-1].total_number_of_chips - result[0].total_number_of_chips)
+    gain_per_round = number_of_chips_gained / total_number_of_rounds
+    print(f'rounds: {total_number_of_rounds}')
+    print(f'gain: {number_of_chips_gained} chips')
+    print(f'gain/round: {gain_per_round:.5f} chips/round')
+    print(f'base bet: {game.myself.strategy.base_bet} chips')
+
+    # todo: I suspect that the basic strategy (or something else) is not implemented correctly, because based on the
+    #  simulation we're losing 0.49 chips/round when we have 2 chips as a bet per round
 
     # fig, ax = plt.subplots(1, 1, figsize=(8, 5))
     # ax.plot([l.round for l in result], [l.total_number_of_chips for l in result])
